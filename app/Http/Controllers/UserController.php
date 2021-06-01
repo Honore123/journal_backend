41aebe08efa570689595cdc6ce0e2e23d46df6a4
honore123 <imanishimwehono@gmail.com>
2021-06-01 10:09:32 +0200

    Fixing user registration api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,16 @@ class UserController extends Controller
 {
     public function store(Request $request)
     {
+       $fields = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        $user = User::where('email',$fields['email'])->first();
+        if($user){
+            return response([
+                'message' => 'Email already registered'
+            ],401);
+        }
         $request['password'] = Hash::make($request->password);
 
         return User::create($request->all());
